d386e0283833ab019a4f26a26e64fb7a92a84c86
k_yakuu <kyakuss5@gmail.com>
2023-04-27 17:42:00 -0400

    lesson practice

--- src/route/index.js
+++ src/route/index.js
@@ -1137,4 +1137,19 @@ router.get('/facebook', function (req, res) {
 
 // ================================================================
 
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 module.exports = router
